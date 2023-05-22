db081c8f0381850a9f2fa13001b2c3307aa9f44c
user <user@MacBook-Pro-user-2.local>
2023-05-22 09:06:22 +0300

    add solita

---
+++ .solitarc.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const programDir = path.join(__dirname, 'programs', 'ju-core');
+const idlDir = path.join(__dirname, 'target/idl');
+const sdkDir = path.join(__dirname, 'src', 'generated');
+const binaryInstallDir = path.join(__dirname, '.crates');
+
+module.exports = {
+  idlGenerator: 'anchor',
+  programName: 'ju_core',
+  programId: '964vWgVEK9X8ZwZB2HyshFVmHUWbcYpRTnVYz2o3F2Xq',
+  idlDir,
+  sdkDir,
+  binaryInstallDir,
+  programDir,
+};
