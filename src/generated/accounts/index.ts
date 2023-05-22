948305c46c50d5d4b8cff72eed805da6d06aaddd
user <user@MacBook-Pro-user-2.local>
2023-05-22 09:37:39 +0300

    add SDK generated

---
+++ src/generated/accounts/index.ts
@@ -0,0 +1,34 @@
+export * from './Alias'
+export * from './App'
+export * from './CollectionItem'
+export * from './Connection'
+export * from './ExternalProcessorPDA'
+export * from './Profile'
+export * from './Publication'
+export * from './Reaction'
+export * from './Report'
+export * from './Subspace'
+
+import { ExternalProcessorPDA } from './ExternalProcessorPDA'
+import { App } from './App'
+import { Profile } from './Profile'
+import { Subspace } from './Subspace'
+import { Publication } from './Publication'
+import { Connection } from './Connection'
+import { CollectionItem } from './CollectionItem'
+import { Alias } from './Alias'
+import { Reaction } from './Reaction'
+import { Report } from './Report'
+
+export const accountProviders = {
+  ExternalProcessorPDA,
+  App,
+  Profile,
+  Subspace,
+  Publication,
+  Connection,
+  CollectionItem,
+  Alias,
+  Reaction,
+  Report,
+}
